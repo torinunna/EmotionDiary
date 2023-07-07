3b2f816cb84f37acc18b3e909dae2684538ad89c
torinunna <torinunna9588@gmail.com>
2023-07-08 00:03:27 +0900

    rename ContentView to DiaryListView

--- EmotionDiary/Diary List/DiaryListView.swift
+++ EmotionDiary/Diary List/DiaryListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  DiaryListView.swift
 //  EmotionDiary
 //
 //  Created by YUJIN KWON on 2023/07/08.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DiaryListView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        DiaryListView()
     }
 }
