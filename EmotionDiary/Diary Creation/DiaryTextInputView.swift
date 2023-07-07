cc20e21e2030eabfb6b15b3d4eb6c042dfb4ad6d
torinunna <torinunna9588@gmail.com>
2023-07-08 00:05:35 +0900

    create Diary(Date/Mood/Text)InputView

---
+++ EmotionDiary/Diary Creation/DiaryTextInputView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryTextInputView.swift
+//  EmotionDiary
+//
+//  Created by YUJIN KWON on 2023/07/08.
+//
+
+import SwiftUI
+
+struct DiaryTextInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiaryTextInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryTextInputView()
+    }
+}
