c805813a10c34284034f0d49de07522cdc5a299d
torinunna <torinunna9588@gmail.com>
2023-07-08 00:04:22 +0900

    create DiaryDetailView

---
+++ EmotionDiary/Diary Detail/DiaryDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryDetailView.swift
+//  EmotionDiary
+//
+//  Created by YUJIN KWON on 2023/07/08.
+//
+
+import SwiftUI
+
+struct DiaryDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiaryDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryDetailView()
+    }
+}
