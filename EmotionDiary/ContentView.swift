0c153388cb2a23397991ca421474cb400c28722b
torinunna <torinunna9588@gmail.com>
2023-07-08 00:02:39 +0900

    Initial Commit

---
+++ EmotionDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmotionDiary
+//
+//  Created by YUJIN KWON on 2023/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmotionDiary/EmotionDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionDiaryApp.swift
+//  EmotionDiary
+//
+//  Created by YUJIN KWON on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct EmotionDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
