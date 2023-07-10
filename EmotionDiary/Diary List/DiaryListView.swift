311cfd5a79dd8e655a81d7c520b80ba3a18cada7
torinunna <torinunna9588@gmail.com>
2023-07-10 15:44:41 +0900

    set navigation title

--- EmotionDiary/Diary List/DiaryListView.swift
+++ EmotionDiary/Diary List/DiaryListView.swift
@@ -20,25 +20,28 @@ struct DiaryListView: View {
     ]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: layout) {
-                ForEach(vm.keys, id:\.self) {
-                    key in
-                    Section {
-                        let items = vm.dic[key] ?? []
-                        let orderedItems = items.sorted(by: { $0.date < $1.date })
-                        ForEach(orderedItems) {
-                            item in MoodDiaryCell(diary: item)
-                                .frame(height: 50)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: layout) {
+                    ForEach(vm.keys, id:\.self) {
+                        key in
+                        Section {
+                            let items = vm.dic[key] ?? []
+                            let orderedItems = items.sorted(by: { $0.date < $1.date })
+                            ForEach(orderedItems) {
+                                item in MoodDiaryCell(diary: item)
+                                    .frame(height: 50)
+                            }
+                        } header: {
+                            Text(formattedSectionTitle(key))
+                                .font(.system(size: 25, weight: .bold))
                         }
-                    } header: {
-                        Text(formattedSectionTitle(key))
-                            .font(.system(size: 25, weight: .bold))
+                        .frame(height: 60)
+                        .padding()
                     }
-                    .frame(height: 60)
-                    .padding()
                 }
             }
+            .navigationTitle("Emotion Diary")
         }
     }
 }
