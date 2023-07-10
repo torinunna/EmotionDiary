8ad2f0154808ee39b373b2c87b62bf8a608b7fad
torinunna <torinunna9588@gmail.com>
2023-07-10 16:23:45 +0900

    add scrollView to DiaryDetailView

--- EmotionDiary/Diary Detail/DiaryDetailView.swift
+++ EmotionDiary/Diary Detail/DiaryDetailView.swift
@@ -14,17 +14,20 @@ struct DiaryDetailView: View {
     var diary: MoodDiary
     
     var body: some View {
-        VStack {
-            Text(diary.date)
-                .font(.system(size: 30, weight: .bold))
-            Image(systemName: diary.mood.imageName)
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 0)
-            Text(diary.text)
-                .font(.system(size: 20, weight: .medium))
-                .foregroundColor(colorScheme == .dark ? .white : .black)
+        ScrollView {
+            VStack(spacing: 50) {
+                Text(diary.date)
+                    .font(.system(size: 30, weight: .bold))
+                Image(systemName: diary.mood.imageName)
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 0)
+                    .frame(height: 80)
+                Text(diary.text)
+                    .font(.system(size: 20, weight: .medium))
+                    .foregroundColor(colorScheme == .dark ? .white : .black)
+            }
         }
     }
 }
