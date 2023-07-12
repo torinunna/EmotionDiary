95f1187c3f4d8706e724462f46bc235d0ab99080
torinunna <torinunna9588@gmail.com>
2023-07-12 15:38:36 +0900

    present DiaryTextInputView when DiaryMoodInputView's NEXT button selected

--- EmotionDiary/Diary Creation/DiaryMoodInputView.swift
+++ EmotionDiary/Diary Creation/DiaryMoodInputView.swift
@@ -14,28 +14,45 @@ struct DiaryMoodInputView: View {
     let moods: [Mood] = Mood.allCases
     
     var body: some View {
-        HStack {
-            ForEach(moods, id:\.self) { mood in
-                Button {
-                    print("Mood selected")
-                    vm.mood = mood
-                } label: {
-                    VStack {
-                        Image(systemName: mood.imageName)
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 60)
-                            .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 0)
-                            .padding()
-                        Text(mood.name)
-                            .foregroundColor(Color.gray)
+        VStack {
+            HStack {
+                ForEach(moods, id:\.self) { mood in
+                    Button {
+                        print("Mood selected")
+                        vm.mood = mood
+                    } label: {
+                        VStack {
+                            Image(systemName: mood.imageName)
+                                .resizable()
+                                .renderingMode(.original)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 60)
+                                .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 0)
+                                .padding()
+                            Text(mood.name)
+                                .foregroundColor(Color.gray)
+                        }
+                        .frame(height: 180)
+                        .background(mood == vm.mood ? Color.gray.opacity(0.4) : Color.clear )
+                        .cornerRadius(10)
                     }
-                    .frame(height: 180)
-                    .background(mood == vm.mood ? Color.gray.opacity(0.4) : Color.clear )
-                    .cornerRadius(10)
                 }
             }
+            .padding()
+            
+            Spacer()
+            
+            NavigationLink {
+                DiaryTextInputView()
+            } label: {
+                Text("NEXT")
+                    .frame(width: 150, height: 60)
+                    .foregroundColor(.white)
+                    .background(Color.pink)
+                    .cornerRadius(40)
+            }
+            
+            Spacer()
         }
     }
 }
